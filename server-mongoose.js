09ed1f7c5cc67081e8d0919351924f0337d5af94
Gavinorme <Gorme@email.sc.edu>
2023-11-29 10:19:01 -0500

    added mongodb atlas

--- server-mongoose.js
+++ server-mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-    .connect("mongodb://localhost/players")
+    .connect("mongodb+srv://gorme:Gavinorme@cluster0.s2hjtru.mongodb.net/?retryWrites=true&w=majority")
     .then(() => console.log("Connected to mongodb"))
     .catch((error) => console.log("Couldn't connect to mongodb", error));
 
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 const upload = multer({ dest: __dirname + "/public/images" });
 
 mongoose
-    .connect("mongodb://localhost/players")
+    .connect("mongodb+srv://gorme:Gavinorme@cluster0.s2hjtru.mongodb.net/?retryWrites=true&w=majority")
     .then(() => console.log("Connected to mongodb"))
     .catch((error) => console.log("Couldn't connect to mongodb", error));
 
@@ -19,26 +19,49 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-let players = [
-    {
-        id: 1, name: "Lebron James", position: "Power Forward", team: "Lakers", nickname: "King James", skills: ["Strength, agility and high basketball intelligence"], img: "images/lebron.jpeg", 
-    },
-    {
-        id: 2, name: "Stephen Curry", position: "Power Forward", team: "Warriors", nickname: "Chef Curry", skills: ["Shooting, handling, and passing"], img: "images/steph.jpeg", 
-    },
-    {
-        id: 3, name: "Kevin Durant", position: "Power Forward", team: "Suns", nickname: "EasyMoneySniper", skills: ["Shooting, isolation, and mid-range"], img: "images/kd.jpeg", 
-    },
-    {
-        id: 4, name: "Damian Lillard", position: "Power Forward", team: "Bucks", nickname: "Dame Dolla", skills: ["Clutch, deep 3's, and handling"], img: "images/dame.jpeg", 
-    },
-    {
-        id: 5, name: "Kyrie Irving", position: "Power Forward", team: "Mavricks", nickname: "Uncle Drew", skills: ["Handling, layups, and passing"], img: "images/kyrie.jpeg", 
-    },
-    {
-        id: 6, name: "Nkola Jokic", position: "Power Forward", team: "Nuggets", nickname: "Joker", skills: ["Passing, shooting, and rebounds"], img: "images/jokic.jpeg", 
-    },
-];
+const playerSchema = new mongoose.Schema({
+    name: String,
+    position: String,
+    team: String,
+    nickname: String,
+    skills: [String],
+    img: String,
+    _id: mongoose.SchemaTypes.ObjectId,
+});
+
+const Player = mongoose.model("Player", playerSchema);
+
+
+
+app.get("/api/players", (req, res) => {
+    getPlayers(res);
+});
+
+const getPlayers = async(res) => {
+    const players = await Player.find();
+    res.send(players);
+};
+
+// let players = [
+//     {
+//         id: 1, name: "Lebron James", position: "Power Forward", team: "Lakers", nickname: "King James", skills: ["Strength, agility and high basketball intelligence"], img: "images/lebron.jpeg", 
+//     },
+//     {
+//         id: 2, name: "Stephen Curry", position: "Power Forward", team: "Warriors", nickname: "Chef Curry", skills: ["Shooting, handling, and passing"], img: "images/steph.jpeg", 
+//     },
+//     {
+//         id: 3, name: "Kevin Durant", position: "Power Forward", team: "Suns", nickname: "EasyMoneySniper", skills: ["Shooting, isolation, and mid-range"], img: "images/kd.jpeg", 
+//     },
+//     {
+//         id: 4, name: "Damian Lillard", position: "Power Forward", team: "Bucks", nickname: "Dame Dolla", skills: ["Clutch, deep 3's, and handling"], img: "images/dame.jpeg", 
+//     },
+//     {
+//         id: 5, name: "Kyrie Irving", position: "Power Forward", team: "Mavricks", nickname: "Uncle Drew", skills: ["Handling, layups, and passing"], img: "images/kyrie.jpeg", 
+//     },
+//     {
+//         id: 6, name: "Nkola Jokic", position: "Power Forward", team: "Nuggets", nickname: "Joker", skills: ["Passing, shooting, and rebounds"], img: "images/jokic.jpeg", 
+//     },
+// ];
 
 app.get("/api/players", (req, res) => {
     res.send(players);
@@ -80,6 +103,8 @@ app.post("/api/players", upload.single("img"), (req, res)=> {
     players.push(player);
     res.send(player);
 });
+
+
 
 app.put("/api/players/:id", upload.single("img"), (req, res) => {
     const id = parseInt(req.params.id);
